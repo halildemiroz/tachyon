97ed5183ed4972d6deb372085a4c6dbd1373643c
halildemiroz <hlldmrz007@gmail.com>
2026-03-17 15:17:17 +0300

    indentation

--- benchmark/main.cpp
+++ benchmark/main.cpp
@@ -11,26 +11,19 @@ int main(){
     std::queue<TradeEvent> tradeQueue;
     std::mutex queueMutex;
     
-    // 1. KRONOMETREYİ BAŞLAT
     auto start = std::chrono::high_resolution_clock::now();
 
-    // 2. SUNİ SCOPE YARAT (Thread'leri hapset)
     {
-        // Thread'ler burada doğar ve çalışmaya başlar
         std::jthread t1(makeTrade, std::ref(tradeQueue), std::ref(queueMutex));
         std::jthread t2(Logger, std::ref(tradeQueue), std::ref(queueMutex));
         
-    } // DİKKAT: Bu süslü parantez bittiğinde, main fonksiyonu t1 ve t2'nin İŞİNİ BİTİRMESİNİ BEKLER! 
-      // Logger dosyayı kapatmadan ve thread'ler ölmeden bu satırdan aşağıya inilmez.
+    }
 
-    // 3. KRONOMETREYİ DURDUR
     auto end = std::chrono::high_resolution_clock::now();
     
-    // 4. MATEMATİĞİ YAP
     std::chrono::duration<double> diff = end - start; // Saniye cinsinden süre
     double gecenSure = diff.count();
     
-    // (Eğer 100.000 satırlık dosyayı okutuyorsan)
     double islenenEmir = 100000.0; 
     double saniyedekiHiz = islenenEmir / gecenSure;
 
